19343e5be3eb99c4e581be27405d224ed1540084
Canghaizhifeng <121001029+cjy0812@users.noreply.github.com>
2026-02-01 19:08:39 +0800

    perf：删除其他功能直接自动注入https://i.gkd.li/*
    适合via简单浏览器

--- dist/network-extension.user.js
+++ dist/network-extension.user.js
@@ -1,96 +1,33 @@
 // ==UserScript==
-// @name               network-extension
-// @name:zh-CN         网络扩展
+// @name               network-extension-for-gkd
+// @name:zh-CN         网络扩展 - GKD专用
 // @namespace          https://github.com/lisonge
-// @version            1.0.7
+// @version            1.0.8
 // @author             lisonge
-// @description        Inject GM_XHR to Website
-// @description:zh-CN  注入GM_XHR到网站
+// @description        Inject GM_XHR to GKD Website
+// @description:zh-CN  注入GM_XHR到GKD网站
 // @icon               https://vitejs.dev/logo.svg
 // @homepageURL        https://github.com/gkd-kit/network-extension
-// @match              http://*/*
-// @match              https://*/*
+// @match              https://i.gkd.li/*
 // @connect            *
-// @grant              GM_getValue
-// @grant              GM_registerMenuCommand
-// @grant              GM_setValue
-// @grant              GM_unregisterMenuCommand
 // @grant              GM_xmlhttpRequest
 // @grant              unsafeWindow
 // @noframes
+// @run-at             document-start
 // ==/UserScript==
 
 (function () {
   'use strict';
 
-  var _GM_getValue = /* @__PURE__ */ (() => typeof GM_getValue != "undefined" ? GM_getValue : void 0)();
-  var _GM_registerMenuCommand = /* @__PURE__ */ (() => typeof GM_registerMenuCommand != "undefined" ? GM_registerMenuCommand : void 0)();
-  var _GM_setValue = /* @__PURE__ */ (() => typeof GM_setValue != "undefined" ? GM_setValue : void 0)();
-  var _GM_unregisterMenuCommand = /* @__PURE__ */ (() => typeof GM_unregisterMenuCommand != "undefined" ? GM_unregisterMenuCommand : void 0)();
   var _GM_xmlhttpRequest = /* @__PURE__ */ (() => typeof GM_xmlhttpRequest != "undefined" ? GM_xmlhttpRequest : void 0)();
   var _unsafeWindow = /* @__PURE__ */ (() => typeof unsafeWindow != "undefined" ? unsafeWindow : void 0)();
-  const useCheckedMenu = ({
-    checkedTag = "✅ ",
-    checkedName = "",
-    uncheckedTag = "❎ ",
-    uncheckedName = void 0,
-    initValue = false
-  }) => {
-    let checked = initValue;
-    const trueName = checkedTag + checkedName;
-    const falseName = uncheckedTag + (uncheckedName ?? checkedName);
-    const currentName = () => checked ? trueName : falseName;
-    const register = () => {
-      _GM_registerMenuCommand(currentName(), () => {
-        onChange(!checked);
-      });
-    };
-    const unregister = () => {
-      _GM_unregisterMenuCommand(currentName());
-    };
-    let onChange = (checked2) => {
-      controller.checked = checked2;
-    };
-    const controller = {
-      get checked() {
-        return checked;
-      },
-      set checked(newValue) {
-        if (newValue == checked) return;
-        unregister();
-        checked = newValue;
-        register();
-      },
-      get onChange() {
-        return onChange;
-      },
-      set onChange(newFn) {
-        onChange = newFn;
-      }
-    };
-    register();
-    return controller;
+
+  // 直接注入到目标网站，无需用户手动选择
+  _unsafeWindow.__NetworkExtension__ = {
+    GM_xmlhttpRequest: _GM_xmlhttpRequest
   };
-  const storeKey = `inject_network:` + location.origin;
-  const nameI18n = {
-    "zh-CN": `注入GM_XHR到当前网站`,
-    "": `Inject GM_XHR to Website`
-  };
-  const menu = useCheckedMenu({
-    checkedName: nameI18n[navigator.language] ?? nameI18n[""],
-    initValue: _GM_getValue(storeKey, false)
-  });
-  menu.onChange = (checked) => {
-    menu.checked = checked;
-    _GM_setValue(storeKey, checked);
-    setTimeout(() => {
-      location.reload();
-    });
-  };
-  if (menu.checked) {
-    _unsafeWindow.__NetworkExtension__ = {
-      GM_xmlhttpRequest: _GM_xmlhttpRequest
-    };
-  }
+
+  // 可选：在控制台输出提示信息
+  console.log('Network Extension for GKD has been injected successfully!');
 
 })();
